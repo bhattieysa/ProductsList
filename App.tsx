0bdb7d391c48ac698c9cd8353fabc663f774e8d0
Eysa Azhar <bhattieysa@gmail.com>
2023-10-06 00:00:03 +0100

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+
+function App(): JSX.Element {
+
+
+  return (
+    <SafeAreaView >
+      <View>
+        <Text>Welcome</Text>
+      </View>    
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
